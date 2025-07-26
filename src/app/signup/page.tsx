fbc8b5b6abf3545eed6f7e680e4ec88e94805241
DJCPREMIX <djcpremix@gmail.com>
2025-07-26 15:06:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,12 @@
 import { SignUpForm } from '@/components/auth/SignUpForm';
+import { NoSsr } from '@/components/common/NoSsr';
 
 export default function SignUpPage() {
   return (
     <main className="container flex items-center justify-center py-12 md:py-24">
-      <SignUpForm />
+      <NoSsr>
+        <SignUpForm />
+      </NoSsr>
     </main>
   );
 }
